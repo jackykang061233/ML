d670ad73a5824750f06bde42133d40b583f293ff
Jacky <kangkangjacky@gmail.com>
2023-11-22 13:39:29 +0000

    update hyperparameters for models

--- 信用卡冒用/tests/test_evaluation.py
+++ 信用卡冒用/tests/test_evaluation.py
@@ -7,7 +7,12 @@ from model import __version__ as _version
 from model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config, ROOT
 
 def test_evaluation():
-    pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
-    test_data = pd.read_csv(str(ROOT)+config.app_config.training_data)
-    prediction(pipeline_file_name)
+    pipeline_file_name = "random_forest_output_v0.0.1.pkl"
+    test_data = pd.read_csv(str(ROOT)+config.app_config.val_data)
+    to_drop = config.log_config.to_drop
+    target = config.log_config.target
+    X_test = test_data.drop(to_drop+[target], axis=1)
+    y_test = test_data[target]
+
+    evaluation(pipeline_file_name=pipeline_file_name, test_data=X_test, y_test=y_test)
 
