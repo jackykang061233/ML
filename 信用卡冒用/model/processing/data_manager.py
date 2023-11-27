7504a18a46642e187bf4412d97098caab5b29438
jackykang <kangkangjacky@gmail.com>
2023-11-27 03:03:43 +0000

    new

--- 信用卡冒用/model/processing/data_manager.py
+++ 信用卡冒用/model/processing/data_manager.py
@@ -7,11 +7,59 @@ from imblearn.pipeline import Pipeline
 
 
 from model import __version__ as _version
-from model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config, ML_ROOT
+from model.config.core import DATASET_DIR, TRAINED_MODEL_DIR, config, ML_ROOT, ROOT
 
-def load_dataset(*, file_name: str) -> pd.DataFrame:
-    df = pd.read_csv(Path(f'{DATASET_DIR}/{file_name}'))
-    return df
+def data_prep():
+    df = pd.read_csv(str(ROOT)+config.app_config.training_data)
+
+    to_drop = config.log_config.to_drop
+    target = config.log_config.target
+
+    X = df.drop(to_drop+[target], axis=1)
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=config.log_config.test_size, stratify=y, random_state=config.log_config.random_state)
+
+    # take only partial data to train
+    if config.log_config.samples_to_train_ratio==1:
+        pass
+    else:
+        X_train, _, y_train, _ = train_test_split(X_train, y_train, test_size=1-config.log_config.samples_to_train_ratio, stratify=y_train, random_state=config.log_config.random_state)
+        
+    return X_train, X_test, y_train, y_test
+
+def custom_val_set():
+    df = pd.read_csv(str(ROOT)+config.app_config.training_data)
+
+    to_drop = config.log_config.to_drop
+    target = config.log_config.target
+
+    X = df.drop(to_drop+[target], axis=1)
+    y = df[target]
+
+    X_train, X_val, y_train, y_val= train_test_split(X, y, test_size=config.log_config.test_size, stratify=y, random_state=config.log_config.random_state)
+
+    test = pd.read_csv(str(ROOT)+config.app_config.val_data)
+    X_test = test.drop(to_drop+[target], axis=1)
+    y_test = test[target]
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def public_as_val():
+    df = pd.read_csv(str(ROOT)+config.app_config.training_data)
+
+    to_drop = config.log_config.to_drop
+    target = config.log_config.target
+
+    X = df.drop(to_drop+[target], axis=1)
+    y = df[target]
+
+
+    test = pd.read_csv(str(ROOT)+config.app_config.val_data)
+    X_test = test.drop(to_drop+[target], axis=1)
+    y_test = test[target]
+
+    return X, X_test, y, y_test
 
 def save_pipeline(*, pipeline_to_save: Pipeline) -> None:
     save_file_name = f'{config.app_config.pipeline_save_file}{_version}.pkl'
--- 信用卡冒用/model/training.py
+++ 信用卡冒用/model/training.py
@@ -13,57 +13,7 @@ from model import __version__ as _version
 # scikit-learn
 from sklearn.model_selection import train_test_split
 
-def data_prep():
-    df = pd.read_csv(str(ROOT)+config.app_config.training_data)
 
-    to_drop = config.log_config.to_drop
-    target = config.log_config.target
-
-    X = df.drop(to_drop+[target], axis=1)
-    y = df[target]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=config.log_config.test_size, stratify=y, random_state=config.log_config.random_state)
-
-    # take only partial data to train
-    if config.log_config.samples_to_train_ratio==1:
-        pass
-    else:
-        X_train, _, y_train, _ = train_test_split(X_train, y_train, test_size=1-config.log_config.samples_to_train_ratio, stratify=y_train, random_state=config.log_config.random_state)
-        
-    return X_train, X_test, y_train, y_test
-
-def custom_val_set():
-    df = pd.read_csv(str(ROOT)+config.app_config.training_data)
-
-    to_drop = config.log_config.to_drop
-    target = config.log_config.target
-
-    X = df.drop(to_drop+[target], axis=1)
-    y = df[target]
-
-    X_train, X_val, y_train, y_val= train_test_split(X, y, test_size=config.log_config.test_size, stratify=y, random_state=config.log_config.random_state)
-
-    test = pd.read_csv(str(ROOT)+config.app_config.val_data)
-    X_test = test.drop(to_drop+[target], axis=1)
-    y_test = test[target]
-
-    return X_train, X_val, X_test, y_train, y_val, y_test
-
-def public_as_val():
-    df = pd.read_csv(str(ROOT)+config.app_config.training_data)
-
-    to_drop = config.log_config.to_drop
-    target = config.log_config.target
-
-    X = df.drop(to_drop+[target], axis=1)
-    y = df[target]
-
-
-    test = pd.read_csv(str(ROOT)+config.app_config.val_data)
-    X_test = test.drop(to_drop+[target], axis=1)
-    y_test = test[target]
-
-    return X, X_test, y, y_test
     
 def train():
     #X_train, X_test, y_train, y_test = custom_val_set()
@@ -89,13 +39,7 @@ def train_cross_val():
     accuracy_lst, precision_lst, recall_lst, f1_lst, auc_lst = cross_validation(X_train, y_train)
         
     print('--------END TRAINING--------')
-    # fig, ax = plt.subplots(1, 5, figsize=(25, 5))
-    # ax[0].plot(accuracy_lst)
-    # ax[1].plot(precision_lst)
-    # ax[2].plot(recall_lst)
-    # ax[3].plot(f1_lst)
-    # ax[4].plot(auc_lst)
-    # plt.show()
+    
 
 def train_grid_search():
     #X_train, X_test, y_train, y_test = data_prep()
